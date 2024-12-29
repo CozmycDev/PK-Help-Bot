49a289d5c7d80b4348f81ae127595203c155bb5b
Cozmyc <172064888+CozmycDev@users.noreply.github.com>
2024-12-29 11:06:47 -0500

    Update config.js

--- core/config.js
+++ core/config.js
@@ -4,7 +4,8 @@ var config = {};
 
 const file = "./config/config.json";
 const tokenFile = "./config/token.txt";
-const openaiTokenFile = './config/openai_token.txt'
+//const openaiTokenFile = './config/openai_token.txt'
+const anthropicTokenFile = './config/anthropic_token.txt'
 
 //Get document, or throw exception on error
 
@@ -27,17 +28,31 @@ function load() {
 		config.Token = token;
 
 		// load openai api key for stack trace analyzer
+                // commented to suppress warnings
+		// if (!fs.existsSync(openaiTokenFile)) {
+		// 	console.log(`No openai token file found! Please insert your api key into ${openaiTokenFile}!`);
+		// 	fs.appendFileSync(openaiTokenFile, "Please insert your api key here!", "utf8");
+		// 	return false;
+		// }
+		// let openaiToken = fs.readFileSync(openaiTokenFile, 'utf8');
+		// if (openaiToken.indexOf(" ") > -1 || token == "" || token.length < 10) {
+		// 	console.log(`You haven't provided your openai api key! Please insert it into ${openaiTokenFile}!`);
+		// 	return false;
+		// }
+		// config.OpenAIToken = openaiToken;
+		
+		// load anthropic api key for stack trace analyzer
 		if (!fs.existsSync(openaiTokenFile)) {
-			console.log(`No openai token file found! Please insert your api key into ${openaiTokenFile}!`);
-			fs.appendFileSync(openaiTokenFile, "Please insert your api key here!", "utf8");
+			console.log(`No anthropic token file found! Please insert your api key into ${anthropicTokenFile}!`);
+			fs.appendFileSync(anthropicTokenFile, "Please insert your api key here!", "utf8");
 			return false;
 		}
-		let openaiToken = fs.readFileSync(openaiTokenFile, 'utf8');
-		if (openaiToken.indexOf(" ") > -1 || token == "" || token.length < 10) {
-			console.log(`You haven't provided your openai api key! Please insert it into ${openaiTokenFile}!`);
+		let anthropicToken = fs.readFileSync(anthropicokenFile, 'utf8');
+		if (anthropicToken.indexOf(" ") > -1 || token == "" || token.length < 10) {
+			console.log(`You haven't provided your anthropic api key! Please insert it into ${anthropicTokenFile}!`);
 			return false;
 		}
-		config.OpenAIToken = openaiToken;
+		config.AnthropicToken = anthropicToken;
 	} catch (e) {
 		console.log(e);
 		return false;
@@ -85,8 +100,12 @@ function getToken() {
 	return typeof config.Token === 'undefined' ? "" : config.Token;
 }
 
-function getOpenAIToken() {
-	return typeof config.OpenAIToken === 'undefined' ? "" : config.OpenAIToken;
+// function getOpenAIToken() {
+// 	return typeof config.OpenAIToken === 'undefined' ? "" : config.OpenAIToken;
+// }
+
+function getAnthropicToken() {
+	return typeof config.AnthropicToken === 'undefined' ? "" : config.AnthropicToken;
 }
 
 function getSplit() {
@@ -108,7 +127,7 @@ function getRoles(group) {
 //ES6
 module.exports = {
 	getToken,
-	getOpenAIToken,
+	getAnthropicToken,
 	getRoles,
 	getSplit,
 	getIgnoredChannels,
